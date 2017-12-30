ebb7e1cda560cca1596ccdefc3d37d586c829884
larce <luis.arce22@gmail.com>
2017-12-29 20:17:00 -0400

    Init resume builder API

---
+++ src/main/java/com/example/rbsbjava/RbSbjavaApplication.java
@@ -0,0 +1,12 @@
+package com.example.rbsbjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbSbjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbSbjavaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/rbsbjava/RbSbjavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.rbsbjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RbSbjavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
